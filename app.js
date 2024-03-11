60972364c94f6aab7a05dad35c6ce7889d640102
sophia kim <sophkim26@bergen.org>
2024-03-11 15:43:11 -0400

    d

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const express = require( "express" );
 const app = express();
 const port = 3000;
 
-// define a route for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "<h1>Hello world!</h1>" );
-} );
-
 // start the server
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
