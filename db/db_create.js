5852650da00abf1d5fda9cfc0423aa73ee02142d
sylviaalee <sylvieelee@gmail.com>
2024-03-12 21:34:14 -0400

    idk if this will work

--- db/db_create.js
+++ db/db_create.js
@@ -10,7 +10,25 @@ const drop_subjects_table_sql = "DROP TABLE IF EXISTS subjects;"
 
 db.execute(drop_subjects_table_sql);
 
+const create_type_table_sql = `
+    CREATE TABLE type (
+        type_id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(45) NOT NULL,
+        PRIMARY KEY (type_id));
+`
+db.execute(create_type_table_sql);
 
+const create_item_table_sql = `
+    CREATE TABLE item (
+        item_id INT NOT NULL AUTO_INCREMENT,
+        author VARCHAR(45) NULL,
+        desc VARCHAR(150) NULL,
+        name VARCHAR(150) NULL,
+        PRIMARY KEY (item_id),
+        FOREIGN KEY (type_id)
+        REFERENCES type (type_id);
+`
+db.execute(create_item_table_sql);
 
 
 db.end();
