2160e59e3604b8dd24ec8d585d3635b1e4d2c8f5
sophia kim <sophkim26@bergen.org>
2024-03-12 20:42:50 -0400

    d

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const mysql = require('mysql2');
 
+// TODO
 const dbConfig = {
     host: "<hostname>",
     port: 3306,
@@ -8,6 +12,16 @@ const dbConfig = {
     database: "<schema>",
     connectTimeout: 10000
 }
+
+// replace dbConfig with this later
+// const dbConfig = {
+//     host: process.env.DB_HOST || "localhost",
+//     port: parseInt(process.env.DB_PORT || "3306"),
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT || "10000")
+// }
 
 const connection = mysql.createConnection(dbConfig);
 
